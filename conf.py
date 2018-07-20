976e5421aacc532c5c15f06d991dd6ab314159fc
bedriy <denys.bedrii@ria.com>
2018-07-20 12:52:54 +0300

    new docs

--- conf.py
+++ conf.py
@@ -109,7 +109,7 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {
+ html_theme_options = {
    'canonical_url': '',
       'analytics_id': '',
       'logo_only': False,
